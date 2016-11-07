67bb21b97c1b4d2944829de4824e8fccc5a8d0a1
TUM-E21-ThinFilms <alexander.book@frm2.tum.de>
2016-11-07 14:51:01 +0100

    Improving factory

--- adl_x547/factory.py
+++ adl_x547/factory.py
@@ -3,17 +3,25 @@ from driver import ADLSputterDriver
 from slave.transport import Serial
 import logging
 
-logger = logging.getLogger('ADL Sputter')
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh = logging.FileHandler('adlsputter.log')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-logger.addHandler(fh)
+
 
 class ADLSputterFactory:
-    def create_sputter(self):
+    
+    def get_logger(self):
+        logger = logging.getLogger('ADL Sputter')
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh = logging.FileHandler('adlsputter.log')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+        return logger
+    
+    def create_sputter(self, device='/dev/ttyUSB11', logger=None):
+        if logger is None:
+            logger = self.get_logger()
+            
         protocol = ADLProtocol(logger=logger)
         protocol.set_name("ADL x.547 Sputter")
-        return ADLSputterDriver(Serial('/dev/ttyUSB11', 9600, 8, 'E', 1, 0.05), protocol)
+        return ADLSputterDriver(Serial(device, 9600, 8, 'E', 1, 0.05), protocol)
 
