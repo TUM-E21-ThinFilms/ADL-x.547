289df028b397edb4231f5e764e46bcde296b0c36
TUM-E21-ThinFilms <alexander.book@frm2.tum.de>
2016-11-16 14:08:32 +0100

    refactoring

--- adl_x547/driver.py
+++ adl_x547/driver.py
@@ -14,11 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import time
-import threading
-
 from slave.driver import Driver, Command
-from slave.types import String, BitSequence
 from protocol import ADLProtocol
 
 from messages.status import *
@@ -39,12 +35,36 @@ from messages.currentcontrol import *
 from messages.powercontrol import *
 
 class ADLSputterDriver(Driver):
-    def __init__(self, transport, protocol=None):
+
+    MODE_VOLTAGE = 1
+    MODE_CURRENT = 2
+    MODE_POWER   = 3
+
+    ADL_GS_05_1000_VOLTAGE = 1000
+    ADL_GS_05_1000_POWER = 500
+    ADL_GS_05_1000_CURRENT = 0.9
+
+    MODEL_ADL_GS_05_1000 = 'GS 05/1000'
+
+    def __init__(self, transport, protocol=None, model=None):
         if protocol == None:
             protocol = ADLProtocol()
 
         super(ADLSputterDriver, self).__init__(transport, protocol)
         self.protocol = protocol
+
+        if model is None:
+            model=self.MODEL_ADL_GS_05_1000
+
+        self.max_power, self.max_voltage, self.max_current = None, None, None
+        self.set_model(model)
+
+    def set_model(self, model):
+        if model is self.MODEL_ADL_GS_05_1000:
+            self.max_power, self.max_voltage, self.max_current = self.ADL_GS_05_1000_POWER, self.ADL_GS_05_1000_VOLTAGE, self.ADL_GS_05_1000_CURRENT
+            return True
+
+        raise ValueError("Unknown model %s" % model)
 
     def send_message(self, message):
         return self._protocol.query(self._transport, message)
@@ -76,7 +96,7 @@ class ADLSputterDriver(Driver):
     def get_ramp(self):
         msg = ReadRampMessage()
         return self.send_message(msg)
-	
+
     def activate_ramp(self):
         msg = ActivateRampMessage()
         return self.send_message(msg)
@@ -105,10 +125,26 @@ class ADLSputterDriver(Driver):
         msg = ReadTargetValueMessage()
         return self.send_message(msg)
 
-    def get_actual_value(self):
-        msg = ActualValueMessage()
-        return self.send_message(msg)
- 
+    def set_mode(self, mode, value, convert=True):
+        if mode not in [self.MODE_CURRENT, self.MODE_POWER, self.MODE_VOLTAGE]:
+            raise ValueError("mode must be either current, power or voltage")
+
+        if mode is self.MODE_CURRENT:
+            if convert:
+                value = self.convert_into_current(value)
+
+            return self.set_mode_i(value)
+        elif mode is self.MODE_POWER:
+            if convert:
+                value = self.convert_into_power(value)
+
+            return self.set_mode_p(value)
+        else:
+            if convert:
+                value = self.convert_into_voltage(value)
+
+            return self.set_mode_u(value)
+
     def set_mode_u(self, voltage):
         msg = VoltageControlMessage()
         msg.set_voltage(voltage)
@@ -124,11 +160,17 @@ class ADLSputterDriver(Driver):
         msg.set_power(power)
         return self.send_message(msg)
 
-    def convert_into_voltage(self, voltage, max_voltage=1000, coeff=4095):
+    def convert_into_voltage(self, voltage, max_voltage=None, coeff=4095):
+        if max_voltage is None:
+            max_voltage = self.max_voltage
         return int(float(voltage)/max_voltage * coeff)
     
-    def convert_into_power(self, power, max_power=500, coeff=4095):
+    def convert_into_power(self, power, max_power=None, coeff=4095):
+        if max_power is None:
+            max_power = self.max_power
         return int(float(power)/max_power * coeff)
     
-    def convert_into_current(self, current, max_current=0.9, coeff=4095):
+    def convert_into_current(self, current, max_current=None, coeff=4095):
+        if max_current is None:
+            max_current = self.max_current
         return int(float(current)/max_current * coeff)
